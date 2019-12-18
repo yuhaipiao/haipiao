ab62f2bcf23954a0d31767a99a7d5a3d4c36db70
gluckwangshun_v <gluckwangshun_v@didiglobal.com>
2019-12-18 15:16:27 +0800

    add sql-获取粉丝最多的前100位

--- persist/src/main/java/com/haipiao/persist/repository/UserRepository.java
+++ persist/src/main/java/com/haipiao/persist/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.haipiao.persist.repository;
 
 import com.haipiao.persist.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -13,6 +14,8 @@ public interface UserRepository extends CrudRepository<User, Integer> {
      * 获取粉丝最多的前100位
      * @return
      */
+    @Query(value = "select * from hp_user where user_id in " +
+            "(select user_id from user_following_relation GROUP BY user_id order by count(user_id) desc limit 100)", nativeQuery = true)
     List<User> getUserByFansCount();
 
 }
