ec2b6f20c8d952f57e4eb68a32438ecff09f4253
Zhongyi Fan <zhongyifan@zhongyidembp.lan>
2020-01-21 22:47:04 -0800

    address code view comments

--- article-service/build-image.sh
+++ article-service/build-image.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-cp ../deployment/config/shared/log4j2.xml .
 ../gradlew build
 docker build -t haipiaodev/article-service:0.0.1 .
-rm ./log4j2.xml
--- user-service/build-image.sh
+++ user-service/build-image.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-cp ../deployment/config/shared/log4j2.xml .
 ../gradlew build
 docker build -t haipiaodev/user-service:0.0.6 .
-rm ./log4j2.xml
